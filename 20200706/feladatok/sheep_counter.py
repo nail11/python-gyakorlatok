4fc1a479294cadc040c5f9970cbae079462f305c
Nagy László <nail@t-online.hu>
2021-07-08 15:40:54 +0200

    sheep_counter kész

--- 20200706/feladatok/sheep_counter.py
+++ 20200706/feladatok/sheep_counter.py
@@ -3,3 +3,13 @@
 # Example:
 # input = 3
 # print out : 1 sheep...2 sheep...3 sheep...
+
+def sheep_counter():
+    n = int(input('Adj meg egy számot: '))
+    l = list(range((n + 1)))
+
+    for i in l[1: len(l) + 1]:
+        print(str(i) + ' bárány', end='...')
+
+
+sheep_counter()
