2b11091ec816bd0134809a4d3e10d6ba0edd7b11
Nagy László <nail@t-online.hu>
2021-07-08 16:08:53 +0200

    single_number kész

--- 20200706/feladatok/single_number.py
+++ 20200706/feladatok/single_number.py
@@ -1,8 +1,20 @@
 # Adott egy egész számokból álló lista. Egy elem kivételével, minden elem kétszer fordul elő a listában.
 # Keresd meg melyik az az elem, ami csak egyszer szerepel a listában.
 # Example 1:
-# Input: nums = [2,2,1]
+# Input: nums =
 # Output: 1
 # Example 2:
 # Input: nums = [4,1,2,1,2]
 # Output: 4
+
+def single_number(nums):
+    for i in nums:
+        if nums.count(i) == 1:
+            print('Ez az elem szerepel egyszer a listában: ' + str(i))
+
+
+nums1 = [2, 2, 1, ]
+nums2 = [4, 1, 2, 1, 2]
+nums3 = [2, 3, 3, 4, 5, 2]
+
+single_number(nums3)
