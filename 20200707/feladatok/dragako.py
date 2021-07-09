4493352fdcba183ce2e8430e5135e9d991f62105
Nagy László <nail@t-online.hu>
2021-07-09 18:44:42 +0200

    2021_07_08

--- 20200707/feladatok/dragako.py
+++ 20200707/feladatok/dragako.py
@@ -1,11 +1,34 @@
 # Betűjelek jelölik az egyes kő típusokat.
-# Minden karakter egy kő fajtának felel meg. A karakterek kis és nagy betű érzékenyek, Tehát az "a" és az "A" különböző típusokat jelölnek.
+# Minden karakter egy kő fajtának felel meg. A karakterek kis és nagy betű érzékenyek, Tehát az "a" és az "A" különböző
+# típusokat jelölnek.
 # A feladat az, hogy megállapítsuk a kövek közül mennyi a drágakő?
 #
 # Example 1:
-# Input: jewels = "aA", stones = "aAAbbbb"
+# Input: jewels = "aA",
 # Output: 3
 #
 # Example 2:
 # Input: jewels = "z", stones = "ZZ"
 # Output: 0
+
+jewels1 = "aA"
+stones1 = "aAAbbbb"
+
+jewels2 = "z"
+stones2 = "ZZ"
+
+jewels3 = "zaBc"
+stones3 = "ZZzzAaBBCcza"
+
+
+def count_jewel(jewels, stones):
+
+    db = 0
+
+    for j in jewels:
+        for s in stones:
+            if j == s:
+                db += 1
+    print(db)
+
+count_jewel(jewels3, stones3)
--- 20200707/feladatok/tu_a_szenakazalban.py
+++ 20200707/feladatok/tu_a_szenakazalban.py
@@ -1,6 +1,11 @@
-# írjunk olyna Python függvényt ami megmondja, hogy hol van a tű a szénakazalban. A függvény fogadjon el egy haystack azaz szénakazal paramétert és egy needle argumentumot. Mindkét argumentum legyen szöveg. Az első tartalmazzon egy hosszú, nem üres stringet, a második egy rövidebb nem üres stringet.
+# írjunk olyna Python függvényt ami megmondja, hogy hol van a tű a szénakazalban. A függvény fogadjon el egy haystack
+# azaz szénakazal paramétert és egy needle argumentumot. Mindkét argumentum legyen szöveg. Az első tartalmazzon egy hosszú,
+# nem üres stringet, a második egy rövidebb nem üres stringet.
 #
-# A függvény úgy keresi a tű-t a szánakazalban, hogy a haystack paraméterben kapott string karakterei között próbál egyezést találni a needle paraméterben kapott string karaktereire. Ha benne van a needle string a haystack stringben akkor adja vissza az első olyan indexet ahol kezdődik a tű a szénakazalban. Ha nincs benne a tű a szénakazalban akkor adjon vissza a függvény egy -1 es értéket
+# A függvény úgy keresi a tű-t a szánakazalban, hogy a haystack paraméterben kapott string karakterei között próbál egyezést
+# találni a needle paraméterben kapott string karaktereire.
+# Ha benne van a needle string a haystack stringben akkor adja vissza az első olyan indexet ahol kezdődik a tű a szénakazalban.
+# Ha nincs benne a tű a szénakazalban akkor adjon vissza a függvény egy -1 es értéket
 #
 # Example 1:
 #
@@ -11,3 +16,25 @@
 #
 # Input: haystack = "aaaaa", needle = "bba"
 # Output: -1
+haystack = 'abcdefghijklmnop'
+needle = 'fghij'
+
+def needle_in_haystack(haystack, needle):
+    index = 0
+    index_n = 0
+    letter = ''
+    letter_ = ''
+    print(needle[0])
+
+    for i_n, n in enumerate(needle):
+        index_n = i_n
+        letter_n = n
+
+        for i_h, h in enumerate(haystack):
+            index = i_h
+            letter = h
+            if letter == n:
+
+
+
+needle_in_haystack(haystack, needle)
