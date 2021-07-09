d3ba2250161d9604e07fc0ecfea11404b4b9e2db
Nagy László <nail@t-online.hu>
2021-07-09 14:23:15 +0200

    2021_07_08

---
+++ 20210708/gyakorlás1.py
@@ -0,0 +1,59 @@
+# Adott két lista amiből egy harmadikat kell csinálni, úgy ,hogy az első listából a páratlan indexű elemeket a másodikból a páros indexű elemeket tartalmazza
+# PL:
+
+listOne = [3, 6, 9, 12, 15, 18, 21]
+listTwo = [4, 8, 12, 16, 20, 24, 28]
+
+# Expected Output:
+
+# Element at odd-index positions from list one
+# [6, 12, 18]
+# Element at even-index positions from list two
+# [4, 12, 20, 28]
+# Printing Final third list
+# [6, 12, 18, 4, 12, 20, 28]
+
+oddList = []
+evenList = []
+
+for i, v in enumerate(listOne):
+    if i % 2 != 0:
+        oddList.append(v)
+for i, v in enumerate(listTwo):
+    if i % 2 == 0:
+        evenList.append(v)
+finalList = oddList + evenList
+print(finalList)
+
+# Erika
+istOne = [3, 6, 9, 12, 15, 18, 21]
+listTwo = [4, 8, 12, 16, 20, 24, 28]
+eredmeny = []  # azért, mert kell egy harmadik lista a feladat szerint
+for i, v in enumerate(listOne):
+    if i % 2 != 0:
+        #print(i, v) Ellenőrzésre jó
+        eredmeny.append(v)
+print(eredmeny)   # részeredmény ellenőrzés
+j = len(eredmeny)  # a kapott eredményt eltároljuk egy változóba
+for i, v in enumerate(listTwo):
+    if i % 2 == 0:
+        #print(i, v)
+        eredmeny.append(v)
+print(eredmeny[j:])  # itt meghívjuk az első listáből készített új listát
+print(eredmeny)  # kiírja a végeredményt
+
+
+# ez mástól
+
+# ez nem megy és csak indexxel sem
+
+listOne = [3, 6, 9, 12, 15, 18, 21]
+# listTwo = [4, 8, 12, 16, 20, 24, 28]
+# mixed_list = []
+# for i, v in enumerate(listOne):
+#    if listOne.index(i) % 2 == 1:
+#        mixed_list += listOne[i]
+#for j, w in enumerate(listTwo):
+#    if listTwo.index(i) % 2 == 0:
+#        mixed_list += listTwo[i]
+# print(mixed_list)
---
+++ 20210708/gyakorlás2.py
@@ -0,0 +1,36 @@
+# Az alábbi mintát jelenítsd meg for ciklus segítségével!
+# Minta:
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+
+list = [1, 2, 3, 4, 5]
+list_rev = list[::-1]
+
+for j in range(5):
+    for i in list_rev[j:]:
+        print(i, end='')
+    print()
+
+# oszlop - ez nem megy
+
+# fx = [5, 4, 3, 2, 1]
+# y = []
+# for j in range(5):
+#     y.append(x[j:])
+# print(y)
+# for idx, i in enumerate(y):
+#     for k in range(len(i)):
+#         print(y[idx][k], end=' ')
+#     print()
+
+# ez megy
+
+# x = [5, 4, 3, 2, 1]
+# y = []
+# for j in range(5):
+#         y.append(x[j:])
+#         for i in y:
+#             print(*i)
